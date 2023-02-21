c0ce37e6b409835074e50e3bc341d6e716ecae3b
Surapat Mekvanich <titleyugio@gmail.com>
2023-02-21 15:48:29 -0800

    updated progress..

--- app/serializers/purchase_serializer.rb
+++ app/serializers/purchase_serializer.rb
@@ -1,5 +1,10 @@
 class PurchaseSerializer < ActiveModel::Serializer
-  attributes :id
-  has_one :user
+  attributes :id, :user_id, :product_id #, :purchase_created_at
+  # has_one :user
   has_one :product
+
+  # def purchase_created_at
+  #   self.Product([:created_at])
+  # end
+  
 end
